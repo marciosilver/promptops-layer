62e73fd0c0328bca926630178e5443ef0ef44051
Seu Nome <seuemail@exemplo.com>
2025-07-24 19:22:55 -0300

    Remoção de componentes LWC não utilizados

--- force-app/main/default/lwc/unitTest/unitTest.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, api } from 'lwc';
-import { sum } from './sum';
-export default class UnitTest extends LightningElement {
-  @api unitNumber = sum(2,3);
-}
--- force-app/main/default/lwc/wireCPR/wireCPR.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import { CurrentPageReference } from 'lightning/navigation';
-export default class WireCPR extends LightningElement {
-  @wire(CurrentPageReference) pageRef;
-  get currentPageRef() {
-    return this.pageRef ? JSON.stringify(this.pageRef, null, 2) : '';
-  }
-}
